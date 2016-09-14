a0ab0093e593f5d9a77bd22d11d329853a56fe38
arushiv <arushiv@umich.edu>
2016-09-14 12:36:43 -0400

    Script to perform inner join on a column given two files

---
+++ matchGeneNamesOnTwoColumns.py
@@ -0,0 +1,58 @@
+# Match gene name on provided column, add matching entries into separate files.
+# Ex. binQuintile file at /lab/arushiv/chromatin/2015_12_08_islet_eQTL/eqtl_binnedByIsletGenes/bins.protein_coding.binQuintile.1.byGenes contains gene names that are in Quintile 1
+#!/usr/bin/env python
+
+
+#from __future__ import print_function
+import argparse
+import collections
+import csv
+import gzip
+import pprint
+import re
+import operator
+import glob
+import pandas
+
+
+def splitByQuantile_full(file1, file2, column_1, column_2):
+    df = pandas.merge(file1, file2, how='inner', left_index = column_1, right_index = column_2)
+    return df
+
+def splitByQuantile_partial(file1, file2, column_1, column_2):
+    file1['keyColumn'] = file1.iloc[:, column_1].str.split('.').apply(lambda x: x[0])
+
+    file2['keyColumn'] = file2.iloc[:, column_2].str.split('.').apply(lambda x: x[0])
+    
+    df = pandas.merge(file1, file2, how='inner', on='keyColumn').drop('keyColumn', 1)
+    return df
+
+def writeToFile(df, outputfile):
+    df.to_csv(outputfile, sep='\t', index=False)
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='Perform inner join on a specified column in two files')
+    parser.add_argument('file1', help="""The first file with column to match on, tab delimited, no header""")
+    parser.add_argument('file2', help="""The second file with column to match on, tab delimited, no header""")
+    parser.add_argument('-c1', '--column_1', type=int, default=1, help="""The column number in the first file.""")
+    parser.add_argument('-c2', '--column_2', type=int, default=1, help="""The column number in the second file.""")
+    parser.add_argument('outputfile', help="""Name of the output file.""")
+    parser.add_argument('-matchType', type=str, default="partial", help="""'full' if gene name in two files match exactly. 'partial' if names in both files do not contain .x extensions. Default = 'partial'""")
+
+    args = parser.parse_args()
+
+    file1 = pandas.read_csv(args.file1, sep='\t', header = None)
+    file2 = pandas.read_csv(args.file2, sep='\t', header = None)
+    column_1 = args.column_1 - 1  ## Because Python index starts from 0
+    column_2 = args.column_2 - 1
+    outputfile = args.outputfile
+    matchType = args.matchType
+
+    if matchType == "partial":
+        df = splitByQuantile_partial(file1, file2, column_1, column_2)
+    elif matchType == "full":
+        df = splitByQuantile_full(file1, file2, column_1, column_2)
+
+    writeToFile(df, outputfile)
