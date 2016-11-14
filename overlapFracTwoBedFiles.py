d804cbacecd261f1ae8d9864865051a6408533e9
arushiv <arushiv@umich.edu>
2016-11-14 13:57:21 -0500

    Also print ratio of overlap with length of file 1

--- overlapFracTwoBedFiles.py
+++ overlapFracTwoBedFiles.py
@@ -23,7 +23,7 @@ def getname(s):
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description="""Given two bed files, output name_file_1, length_file_1, name_file_2, length_file_2, overlap.
+    parser = argparse.ArgumentParser(description="""Given two bed files, output name_file_1, length_file_1, name_file_2, length_file_2, length_of_overlap, ratio length_of_overlap/length_file_1.
 Usage:  ~/myEnv/bin/python ~arushiv/toolScripts/overlapFracTwoBedFiles.py a.bed b.bed""")
     parser.add_argument('bedfile1', type=str, help="""bed file 1.""")
     parser.add_argument('bedfile2', type=str, help="""bed file 2.""")
@@ -36,6 +36,11 @@ Usage:  ~/myEnv/bin/python ~arushiv/toolScripts/overlapFracTwoBedFiles.py a.bed 
     bedfile2 = pybedtools.BedTool(args.bedfile2)
     intersect = bedfile1.intersect(bedfile2)
 
-    print getname(args.bedfile1), lengthOfBedFile(bedfile1), getname(args.bedfile2), lengthOfBedFile(bedfile2), lengthOfBedFile(intersect)
+    lengthFile1 = lengthOfBedFile(bedfile1)
+    lengthFile2 = lengthOfBedFile(bedfile2)
+    lengthIntersect = lengthOfBedFile(intersect)
+    nameFile1 = getname(args.bedfile1)
+    nameFile2 = getname(args.bedfile2)
+    print nameFile1, lengthFile1, nameFile2, lengthFile2, lengthIntersect, lengthIntersect/lengthFile1  # getname(args.bedfile1), lengthOfBedFile(bedfile1), getname(args.bedfile2), lengthOfBedFile(bedfile2), lengthOfBedFile(intersect)
     
     
