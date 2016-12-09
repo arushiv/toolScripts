f52e8823178f097d0c0ae3cdfd73399ac97ec169
arushiv <arushiv@umich.edu>
2016-12-09 14:44:12 -0500

    fixes

--- makeDataFrame_gregor.py
+++ makeDataFrame_gregor.py
@@ -4,13 +4,18 @@
 
 import argparse
 import pandas
+import glob
                    
 def makedataframe(filename, fieldSeparator, outputfilename):
+    job_info = filename.replace('/StatisticSummaryFile.txt','').replace('output_','')
+
     df = pandas.read_csv(filename, sep='\t')
     df['Bed_File'] = df['Bed_File'].str.replace('.bed','')
+    df['job_info'] = job_info
     ndf = df['Bed_File'].str.split(fieldSeparator, expand=True)
-    outdf = pandas.concat([ndf, df.drop('Bed_File', 1)], 1)
-
+    jdf = df['job_info'].str.split("_", expand=True)
+    outdf = pandas.concat([jdf, ndf, df.drop(['Bed_File','job_info'], 1)], 1)
+    # print outdf
     outdf.to_csv(outputfilename, mode='a', header=False, index=False, sep='\t', na_rep="NA")
     
 if __name__ == '__main__':
