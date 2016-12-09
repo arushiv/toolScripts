7a029ae3b5046a6b1bcd7f394ed3337d9036a469
arushiv <arushiv@umich.edu>
2016-12-09 13:59:53 -0500

    Now appends dataframe faster using pandas

--- makeDataFrame_gregor.py
+++ makeDataFrame_gregor.py
@@ -1,46 +1,21 @@
 # Run GREGOR: Fix conf files, submit drmr commands, compile dataframe and plot 
 #!/usr/bin/env python
-# 
-# from __future__ import print_function
-import argparse
-import collections
-import csv
-import gzip
-import magic
-import pprint
-import re
-import operator
-import glob
-import math
-import sys
-import subprocess as sp
-import os
-import itertools
-import shutil
-import fileinput
-import fnmatch
 
-# class RunGregor(object):
-#     def __init__(self):
-#         pass
-    
+
+import argparse
+import pandas
+                   
 def makedataframe(filename, fieldSeparator, outputfilename):
-    with open(outputfilename, 'a') as fout:
-        with open(filename) as f:
-            snplist = ((filename.split('/')[0]).replace('output_','')).split('_')
-            snpcolumns = '\t'.join(snplist[0].split('.')+snplist[1:])
-                   
-            for line in f:
-                if "Bed_File" not in line:
-                    s = line.split('\t')
-                    namecolumns = '\t'.join((s[0].replace('.bed','')).split(fieldSeparator))
-                    datacolumn = '\t'.join(s[1:])
-                    fout.write("%s\t%s\t%s"%(snpcolumns,namecolumns,datacolumn))
-                   
+    df = pandas.read_csv(filename, sep='\t')
+    df['Bed_File'] = df['Bed_File'].str.replace('.bed','')
+    ndf = df['Bed_File'].str.split(fieldSeparator, expand=True)
+    outdf = pandas.concat([ndf, df.drop('Bed_File', 1)], 1)
+
+    outdf.to_csv(outputfilename, mode='a', header=False, index=False, sep='\t', na_rep="NA")
     
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Run GREGOR: Fix conf files, submit drmr commands, compile dataframe and plot.')
+    parser = argparse.ArgumentParser(description='Compile dataframe from GREGOR output folders.')
     parser.add_argument('filename', help="""The StatisticSummaryFile from GREGOR.""")
     parser.add_argument('-f','--nameFieldSeparator', type=str, default='.', help="""Field separator to make columns from bed file name. (Default='.')""")
     parser.add_argument('outputfilename', help="""Output file name.""")
