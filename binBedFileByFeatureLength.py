73ed9648b8b64878a562fdb74061da9433faaa41
arushiv <arushiv@umich.edu>
2017-01-17 15:48:03 -0500

    Comments added

--- binBedFileByFeatureLength.py
+++ binBedFileByFeatureLength.py
@@ -9,7 +9,7 @@ import pybedtools as pb
 
 def printFunc(df, outputFile):
         name = "%s.%s.bed" %(outputFile, df.loc[0]['bin'] + 1)
-        df.loc[:,['chrom','start','end','bin']].to_csv(name, sep="\t", index=False, header=False)
+        df.loc[:,['chrom','start','end']].to_csv(name, sep="\t", index=False, header=False)
     
 
 def cutByLength(bedfile, bins, outputFile):
@@ -22,7 +22,7 @@ def cutByLength(bedfile, bins, outputFile):
     
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description="""Bin a bed file based on length of features.)
+    parser = argparse.ArgumentParser(description="""Bin a bed file by ascending length of features. Resulting bins are numbered in ascending order starting from 1 (which implies the samllest feature bin))
 Usage:  python ~arushiv/toolScripts/binBedFileByFeatureLength.py <bedfile> <outname> -b <bins>""")
     parser.add_argument('bedfile', type=str, help="""Bed file to be split into bins by length of features""")
     parser.add_argument('-b', '--bins', type=int, help="""Number of bins""")
