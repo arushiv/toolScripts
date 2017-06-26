a5b80cd449d54f0cce0105b21ea167185127973f
arushiv <arushiv@umich.edu>
2017-06-26 11:54:59 -0400

    small script to make new same name directory under ~home/arushiv or other root name provided, copy a template commands.py file and link to current folder

---
+++ mkcom.sh
@@ -0,0 +1,13 @@
+dir=`pwd`
+newdir=`echo $dir | sed -e 's:/lab/work/arushiv/::g'`
+
+
+if [ $# -eq 0 ]; then
+	dirname="/home/arushiv/$newdir"
+else
+	dirname="${1}/$newdir"
+fi
+    
+mkdir -p $dirname
+cp /home/arushiv/toolScripts/commands.py $dirname
+ln -s $dirname/commands.py .
