3fabf0f8af081c99d3f734cdb7c09cb63218230c
arushiv <arushiv@umich.edu>
2017-09-29 11:23:18 -0400

    format input for fGWAS

--- fGWAS_formatInput.py
+++ fGWAS_formatInput.py
@@ -7,12 +7,12 @@ import os
 def getOpts():
     parser = argparse.ArgumentParser(description = 'Format input file for fGWAS', usage = 'python fGWAS_formatInput.py <gwasData> <annotationDirectory> <outputfile> -f chr:pos')
     parser.add_argument('associationFile', help = """input file for association statistics.""")
-    parser.add_argument('annotationDirectory', help = """Directory with annotation bed files.""")
     parser.add_argument('outputfile', help = """Output filename.""")
     parser.add_argument('-f', '--inputFormat', nargs = '+', help = """format of chrom, pos in input file, followed by column name. Options: chr1:10 colname, 1:10 colname """)
-
-    parser.add_argument('-sa', '--subsetAnnotations', type = str, required = True, help = """Provide wildcard phrase to glob a subset of annotation files from the directory provided. Eg. Islets.*.bed. or *.bed.gz""")
+    parser.add_argument('-adir','--annotationDirectory', help = """Directory with annotation bed files.""")
+    parser.add_argument('-sa', '--subsetAnnotations', type = str, default="*.bed", help = """Provide wildcard phrase to glob a subset of annotation files from the directory provided. Eg. Islets.*.bed. or *.bed.gz""")
     parser.add_argument('-k', '--keepCols', nargs = '+', help = """Provide header names in current association input that correspond to these in the output: SNPID, Z, F, N""")
+    parser.add_argument('-of','--onlyFormat', action='store_const', const="onlyFormat", help="""Only format association file. If the input file is too large, it may be faster to intersect with annotations using bedtools directly rather than within pybedtools. Use this flag in that case.""")
     return parser
 
 def subsetFiles(annotationDirectory, subsetAnnotations):
@@ -20,7 +20,11 @@ def subsetFiles(annotationDirectory, subsetAnnotations):
     return outlist
 
 def formatFile(associationFile, keepCols, inputFormat):
-    d = pandas.read_csv(associationFile, sep='\t')
+    if inputFormat[1] not in keepCols:
+        usecols = inputFormat[1] + keepCols
+    else:
+        usecols = keepCols
+    d = pandas.read_csv(associationFile, sep='\t', usecols=usecols)
     if inputFormat[0] == "chr1:10":
         d.loc[:,'CHR'], d.loc[:,'POS'] = d[inputFormat[1]].str.split(":",2).apply(lambda x: x[:2]).str
         d.loc[:,'POS'] = d['POS'].astype(int)
@@ -29,6 +33,7 @@ def formatFile(associationFile, keepCols, inputFormat):
         d.loc[:,'POS'] = d['POS'].astype(int)
         d.loc[:,'CHR'] = d['CHR'].map(lambda x: "chr{x}".format(x=x))
 
+    d = d[d['POS'] > 0]
     d.loc[:,'start'] = d['POS'] - 1
     neededHeaders = ['SNPID','Z','F','N']
     renameList = dict(zip(keepCols, neededHeaders))
@@ -36,17 +41,19 @@ def formatFile(associationFile, keepCols, inputFormat):
     
     headerlist = ['CHR','start','POS'] + neededHeaders
     d = d[headerlist]
+    print("assocFileFormatted")
     return d
 
 def getIntersections(assoc_df, fileList):
-    assoc_bed = pybedtools.BedTool.from_dataframe(assoc_df)
+    assoc_bed = pybedtools.BedTool.from_dataframe(assoc_df).sort()
 
     for filename in fileList:
         header = os.path.basename(filename).replace(".bed","")
         annot_bed = pybedtools.BedTool(filename)
-        assoc_df.loc[:,header] = assoc_bed.intersect(annot_bed, c=True).to_dataframe().iloc[:,-1]
-    print(assoc_df.head())
-        
+        assoc_df.loc[:,header] = assoc_bed.intersect(annot_bed, c=True, sorted=True).to_dataframe().iloc[:,-1]
+        print(header)
+    return assoc_df
+
 if __name__ == '__main__':
     parser = getOpts()
     args = parser.parse_args()
@@ -57,10 +64,13 @@ if __name__ == '__main__':
     inputFormat = args.inputFormat
     subsetAnnotations = args.subsetAnnotations
     keepCols = args.keepCols
-
-    fileList = subsetFiles(annotationDirectory, subsetAnnotations)
     
-    assoc_df = formatFile(associationFile, keepCols, inputFormat)    
-    dannot = getIntersections(assoc_df, fileList)
+    assoc_df = formatFile(associationFile, keepCols, inputFormat)
     
-    # dannot.to_csv(outputfile, sep='\t', index=False)
+    if args.onlyFormat == "onlyFormat":
+        outdf = pybedtools.BedTool.from_dataframe(assoc_df).sort().to_dataframe()
+        assoc_df.to_csv(outputfile, sep='\t', index=False, compression="gzip")
+    else:
+        fileList = subsetFiles(annotationDirectory, subsetAnnotations)
+        dannot = getIntersections(assoc_df, fileList)
+        dannot.to_csv(outputfile, sep='\t', index=False, compression="gzip")
